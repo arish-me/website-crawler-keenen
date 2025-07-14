74297306fa2235cf7d3d309f5fc7a7fc7310eb5a
Arish <thermic.arish@gmail.com>
2025-07-14 22:21:04 +0530

    Add Fevicon and Logo

--- frontend/src/components/UserSettings/Appearance.tsx
+++ frontend/src/components/UserSettings/Appearance.tsx
@@ -16,7 +16,7 @@ const Appearance = () => {
         <RadioGroup
           onValueChange={(e) => setTheme(e.value)}
           value={theme}
-          colorPalette="teal"
+          colorPalette="orange"
         >
           <Stack>
             <Radio value="system">System</Radio>
